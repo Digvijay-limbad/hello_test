5d91fbe88c1c30e1802051e116f351af4ded036f
root <root@workstation.lab.example.com>
2025-11-02 18:10:56 +0000

    ssamplecode

---
+++ hello.py
@@ -0,0 +1,15 @@
+def factorial_for_loop(n):
+        if n < 0:
+                    return "Factorial is not defined for negative numbers."
+                    elif n == 0:
+                                return 1
+                                else:
+                                            result = 1
+                                                    for i in range(1, n + 1):
+                                                                    result *= i
+                                                                            return result
+
+                                                                        # Example usage
+                                                                        num = 5
+                                                                        print(f"The factorial of {num} using a forrrrrr loop is: {factorial_for_loop(num)}")
+
